b333aa1ee826eca8dc1a4f1b468c26da1aab5e69
Andi Koh <andikoh@dyn1236-142.wlan.ic.ac.uk>
2016-05-21 15:30:10 +0100

    made lots of structs

--- src/emulate.c
+++ src/emulate.c
@@ -2,52 +2,182 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct Arm {
+#define PROCESSING 1
+#define MULTIPLY 2
+#define TRANSFER 3
+#define BRANCH 4
+
+typedef struct _arm *Arm; 
+typedef struct _process *Process; 
+typedef struct _multiply *Multiply; 
+typedef struct _transfer *Transfer; 
+typedef struct _branch *Branch; 
+typedef struct _instruction *Instruction; 
+
+typedef struct _arm {
     uint32_t registers[17];
     //register 15 is PC
-};
+} arm; 
+
+typedef struct _instruction {
+    uint32_t Cond; 
+    Process p; 
+    Multiply m; 
+    Transfer t; 
+    Branch b; 
+} instruction; 
+
+typedef struct _process {
+    uint32_t I; 
+    uint32_t Opcode; 
+    uint32_t S;
+    uint32_t Rn;
+    uint32_t Rd;
+    uint32_t Operand2;
+} process; 
+
+typedef struct _multiply {
+    uint32_t A;
+    uint32_t S; 
+    uint32_t Rd;
+    uint32_t Rn;
+    uint32_t Rs;
+    uint32_t Rm;
+} multiply; 
+
+typedef struct Transfer {
+    uint32_t I;
+    uint32_t P; 
+    uint32_t U; 
+    uint32_t L; 
+    uint32_t Rn;
+    uint32_t Rd;
+    uint32_t Offset; 
+}
+
+typedef struct _branch {
+    uint32_t Offset; 
+} branch; 
 
 FEcycle() {
-    Cond 
-    IO
-    int Opcode; //1234
-    Sc
-    Rn
-    Rd
-    Rm
-    Rs
-    O2
+    Instruction components = malloc (sizeof (struct _instruction));
+    Instruction -> cond = (0xF0000000 & instruction) >> 28; 
+    if (checkCond (a, cond)) {
+        int type = decode(components, fetch (a)); //initialises relevant values 
+    
+        switch(type) {
+            1: executeP(components -> p);
+            2: executeM(components -> m);
+            3: executeT(components -> t);
+            4: executeB(components -> b);
+        }
+    } else {
+        //next instruction 
+    }
+    
+}
 
-    decode(); //initialises relevant values 
-    switch(Opcode) {
-        1: execute1()
-        2: execute2()
-        3: execute3()
-        4: execute4()
+void executeP(Process p) {
+
+}
+
+void executeM(Multiply p) {
+
+}
+
+void executeT(Transfer t) {
+
+}
+
+void executeB(Branch b) {
+
+}
+
+bool checkCond(Arm a, uint32_t cond) {
+    uint32_t CPSR = (a -> registers[16] & mask) >> 28;
+    
+    uint32_t n = (0x00000008 & CPSR) >> 3;
+    uint32_t z = (0x00000004 & CPSR) >> 2; 
+    uint32_t c = (0x00000002 & CPSR) >> 1;
+    uint32_t v = 0x00000001 & CPSR; 
+
+    switch(cond) {
+        0: return z == 1; 
+        1: return z == 0; 
+        10: return n == v; 
+        11: return n != v; 
+        12: return z == 0 && n == v; 
+        13: return z == 1 || n != v; 
+        14: return true; 
     }
 }
 
-uint32_t fetch(Arm a) {
+uint32_t fetch(Arm a) { //returns instruction 
     //increment PC
-    uint32_t PC = a.registers[15];
+    uint32_t PC = a -> registers[15];
     //convert PC from binary to integer rep 
     //get value at memory 
     PC++;
     return memory[(int) (PC-1)];
 }
 
-int decode(uint32_t instruction) {
-
-}
-
-execute (int action) {
-    switch (action) {
-        
+int decode(Instruction components, uint32_t instruction) {
+    //take 2 
+    uint32_t mask = 0x0C000000; 
+    if ((instuction&mask) >> 26 == 1) {
+        //single data transfer
+        components -> t = malloc (sizeof (struct _transfer));
+        decodeT(t, instruction);
+        return TRANSFER; 
+    } else if (instruction&mask >> 26 == 2) {
+        //branch 
+        components -> b = malloc (sizeof (struct _branch));
+        decodeB(b, instruction);
+        return BRANCH; 
+    } else {
+        mask = 0x02000000; 
+        if (instruction&mask == 1) {
+            //data processing
+            components -> p = malloc (sizeof (struct _processing));
+            decodeP(p, instruction);
+            return PROCESSING;
+        } else { //they are both 000
+            mask = 0x000000F0; 
+            if (instruction&mask == 9) {
+                //multiply 
+                components -> m = malloc (sizeof (struct _multiply));
+                decodeM(m, instruction);
+                return MULTIPLY;
+            } else {
+                //data processing
+                components -> p = malloc (sizeof (struct _processing));
+                decodeP(p, instruction);
+                return PROCESSING;
+            }
+        }
     }
 }
 
-
+void decodeT(Transfer t, uint32_t instruction) {
+    
+    t -> Cond = (0xF0000000 & instruction) >> 28;
+    t -> I = 
+    t -> P = 
 }
+
+void decodeP(Process p, uint32_t instruction) {
+    p -> Cond = (0xF0000000 & instruction) >> 28;
+}
+
+void decodeM(Multiply m, uint32_t instruction) {
+    m -> Cond = (0xF0000000 & instruction) >> 28;
+}
+
+void decodeB(Branch b, uint32_t instruction) {
+    b -> Cond = (0xF0000000 & instruction) >> 28;
+}
+
+
 
 
 int destination; 
