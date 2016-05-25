fd9fb2006d550e1d4f79cf10d35c6d07aec9f1a5
tsd15 <tsd15@ic.ac.uk>
2016-05-25 17:02:15 +0100

    Starting Assembler: Processing

--- src/assemble.c
+++ src/assemble.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 typedef struct _lable *LabelNode;
 typedef struct _ass *Ass;
+typedef struct _process *Process;
 
 #define PROCESSING 1
 #define MULTIPLY 2
@@ -21,6 +22,16 @@ typedef struct _label {
   int p;
   LabelNode next;
 };
+
+typedef struct _process {
+    uint32_t Cond;
+    uint32_t I;
+    uint32_t Opcode;
+    uint32_t S;
+    uint32_t Rn;
+    uint32_t Rd;
+    uint32_t Operand2;
+} ;
 
 // helper function to add a new label to the symbol table(using linked list)
 void push(node_t * head, char *label, int position) {
@@ -85,6 +96,7 @@ int identify(char *instruction) {
   }
 }
 
+//input "r1" "r2" "r3" and output 1,2,3 in binary
 uint32_t regTrans(char *reg) {
     if (reg[0] == '\n') { //remove the letter 'r' from r1, r2, ...
             memmove(reg, reg+1, strlen(reg));
@@ -92,7 +104,7 @@ uint32_t regTrans(char *reg) {
     int num = atoi(reg);//get the number in integer
     return (uint32_t) num;
 }
-
+// reorder the instruction to little-endian
 uint32_t reorder(uint32_t ins) {
     uint32_t first = ins >> 24;//take the first byte
     uint32_t second = ((ins >> 16) & ~(1<<8)) << 8;//take the second byte
@@ -102,10 +114,64 @@ uint32_t reorder(uint32_t ins) {
 
 }
 
+void opCodeP(char *mnemonic, PROCESSING p) {
+    p->S = 0 << 20;
+    p->Cond = 14 << 28;
+    if (strcmp(mnemonic,"and") == 0) {
+        p->Opcode = 0;
+    } else if (strcmp(mnemonic,"eor") == 0) {
+        p->Opcode = 1;
+    } else if (strcmp(mnemonic,"sub") == 0) {
+        p->Opcode = 2;
+    } else if (strcmp(mnemonic,"rsb") == 0) {
+        p->Opcode = 3;
+    } else if (strcmp(mnemonic,"add") == 0) {
+        p->Opcode = 4;
+    } else if (strcmp(mnemonic,"orr") == 0) {
+        p->Opcode = 12;
+    } else if (strcmp(mnemonic,"mov") == 0) {
+        p->Opcode = 13;
+    } else if (strcmp(mnemonic,"tst") == 0) {
+        p->Opcode = 8;
+        p->S = 1;
+    } else if (strcmp(mnemonic,"teq") == 0) {
+        p->Opcode = 9;
+        p->S = 1 << 20;
+    } else {
+        p->Opcode = 10;
+        p->S = 1 << 20;
+    }
+    p->Opcode <<= 21;
+}
+
+void operand2Handler(char *operand2, char *rest, PROCESSING p) {
+    if (rest == NULL) {
+        if (operand2[0] == 'r' ) {
+            p->Operand2 = regTrans(operand2);
+            p->I = 0;
+        } else if (operand[0] == '#') {
+            p->Operand2 = regTrans(operand2);
+            P->I = 1 << 25;
+        }
+    } else {
+
+    }
+
+}
+//Translate Processing
 void translateP(char *ins, Ass a, int pos) {
+    PROCESSING p = malloc(sizeof (struct _process));
+    char *mnemonic = strtok(ins," ");
+    opCodeP(mnemonic, p);
+    p->Rn = regTrans(strtok(NULL," ")) << 16;
+    p->Rd = regTrans(strtok(NULL," ")) << 12;
+    char *operand2 = strtok(NULL," ");
+    char *rest = strtok(NULL," ");
+
 
 }
 
+//Translate Multiply
 void translateM(char *ins, Ass a, int pos) {
     uint32_t Cond = 14 << 28; // Condition field is 1110
     uint32_t S = 0; // S field is 0
@@ -130,36 +196,12 @@ void translateM(char *ins, Ass a, int pos) {
     a->memory[pos] = reorder(result);
 }
 
+//Translate Transfer
 void translateT(char *ins, Ass a, int pos) {
 
 }
 
-void translateB(char *ins, Ass a, int pos) {
-
-}
-
-void translateS(char *ins, Ass a, int pos) {
-
-}
-
-void translate(Ass a, FILE fr, LabelNode head) {
-  char *next;
-  int position = 0;
-  while (fgets(next, MAX_CHARS, fr)) {
-    char *newtemp = calloc(strlen(next) + 1, sizeof(char));
-    strcpy(newtemp, next);
-    char *mnemonic = strtok(newtemp," ");
-    switch(identify(mnemonic)) {
-      case 1: translateP(next, a, position); position += 4; break;
-      case 2: translateM(next, a, position); position += 4; break;
-      case 3: translateT(next, a, position); position += 4; break;
-      case 4: translateB(next, a, position); position += 4; break;
-      case 5: translateS(next, a, position); position += 4; break;
-      case 6: break;
-    }
-  }
-}
-
+//Translate Branch
 void translateB (char *line, Ass a, int position) {
   //Get the cond
   char *cond = strtok(line," ");
@@ -186,6 +228,32 @@ void translateB (char *line, Ass a, int position) {
   int offset = position
 
 }
+
+//Translate Special
+void translateS(char *ins, Ass a, int pos) {
+
+}
+
+
+void translate(Ass a, FILE fr, LabelNode head) {
+  char *next;
+  int position = 0;
+  while (fgets(next, MAX_CHARS, fr)) {
+    char *newtemp = calloc(strlen(next) + 1, sizeof(char));
+    strcpy(newtemp, next);
+    char *mnemonic = strtok(newtemp," ");
+    switch(identify(mnemonic)) {
+      case 1: translateP(next, a, position); position += 4; break;
+      case 2: translateM(next, a, position); position += 4; break;
+      case 3: translateT(next, a, position); position += 4; break;
+      case 4: translateB(next, a, position); position += 4; break;
+      case 5: translateS(next, a, position); position += 4; break;
+      case 6: break;
+    }
+  }
+}
+
+
 
 int main(int argc, char **argv) {
   //open binary file to write
