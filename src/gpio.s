59a366487a3ce8c1cbb5b48c18f22e1cd8c57189
Ioan-Andrei Isaila <ii515@ray01.doc.ic.ac.uk>
2016-06-03 16:57:46 +0100

    assembler code that makes LED nr 18 flash on and off

---
+++ src/gpio.s
@@ -0,0 +1,27 @@
+ldr r0,=0x20200000
+mov r1,#1
+lsl r1,#24
+str r1,[r0,#4]
+
+mov r2,#1
+lsl r2,#18
+
+loop:
+str r2,[r0,#40]
+mov r4,0XFFFF
+
+delay:
+sub r4,r4,#1
+cmp r4,#0
+bne delay
+
+str r2,[r0,#28]
+mov r4,=0XFFFF
+
+delay1:
+sub r4,r4,#1
+cmp r4,#0
+bne delay1
+
+b loop
+
